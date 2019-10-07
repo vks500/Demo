bde41acf00f353a085b465264225e34c4f3150a7
vks500 <56271610+vks500@users.noreply.github.com>
2019-10-07 23:26:35 +0530

    DemoTest

---
+++ Test.java
@@ -0,0 +1,26 @@
+class A{
+	public void f1(){
+		
+	}
+}
+
+class b extends A{
+	
+	public  void f1(){
+}
+
+
+public static class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello");
+		A obj=new A();
+		obj.f1();
+	}
+
+}}
